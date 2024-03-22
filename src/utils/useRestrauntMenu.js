1a06d03488b5853ce4425f00deb7c7d4b793e8ef
Dhaval Gala <galadhaval913@gmail.com>
2024-03-22 16:50:22 +0530

    Swiggy live api structure changes

--- src/utils/useRestrauntMenu.js
+++ src/utils/useRestrauntMenu.js
@@ -11,9 +11,9 @@ const useRestrauntMenu = (id) => {
     const data = await fetch(process.env.REACT_APP_FETCH_RESTRA_MENU + id);
 
     const json = await data.json();
-    setResInfo(json.data.cards[2].card.card.info);
+    setResInfo(json.data.cards[0].card.card.info);
     let filteredCat =
-      json.data.cards[5].groupedCard.cardGroupMap.REGULAR.cards.filter(
+      json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards.filter(
         (resCard) =>
           resCard.card.card?.["@type"] ===
           "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
